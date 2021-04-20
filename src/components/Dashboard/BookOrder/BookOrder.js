cab1c983218b354ce060d07a29d1156d77247ac0
Aktaruzzaman Joti <aktaruzzamanjoti97@gmail.com>
2021-04-20 23:45:51 +0600

    feeling bored

--- src/components/Dashboard/BookOrder/BookOrder.js
+++ src/components/Dashboard/BookOrder/BookOrder.js
@@ -5,43 +5,43 @@ const bookingData = [
     {
         _id: '5e8df50be6e8231764dc23de',
         id: 1,
-        subject: 'Covid-19 test',
-        visitingHour: '8:00 AM - 9:00 AM',
+        subject: 'Electric test',
+
         totalSpace: 10
     },
     {
         _id: '5e8df578e6e8231764dc23df',
         id: 2,
-        subject: 'Blood Pressure measure',
-        visitingHour: '10:50 AM - 11:30 AM',
+        subject: '',
+     
         totalSpace: 10
     },
     {
         _id: '5e8df5aee6e8231764dc23e0',
         id: 3,
-        subject: 'Guideline for Diabatics',
-        visitingHour: '5:00 PM - 6:00 PM',
+        subject: 'Electric Service',
+       
         totalSpace: 10
     },
     {
         _id: '5e8df63be6e8231764dc23e1',
         id: 4,
-        subject: 'Advising for Depression',
-        visitingHour: '7:00 AM - 8:30 AM',
+        subject: 'Plumbing',
+      
         totalSpace: 10
     },
     {
         _id: '5e8df68de6e8231764dc23e2',
         id: 5,
-        subject: 'Teeth Orthodontics',
-        visitingHour: '8:00 AM - 9:00 AM',
+        subject: 'Fridge Service',
+  
         totalSpace: 10
     },
     {
         _id: '5e8df6a0e6e8231764dc23e3',
         id: 6,
-        subject: 'Mental Support',
-        visitingHour: '8:00 AM - 9:00 AM',
+        subject: 'Fan Service',
+ 
         totalSpace: 10
     }
 ]
--- src/components/Dashboard/BookingCard/BookingCard.js
+++ src/components/Dashboard/BookingCard/BookingCard.js
@@ -19,8 +19,8 @@ const BookingCard = ({booking, date}) => {
             <div className="card p-3">
                 <div className="card-body text-center">
                     <h5 className="card-title text-brand">{booking.subject}</h5>
-                    <h6>{booking.visitingHour}</h6>
-                    <p>{booking.totalSpace} SPACES AVAILABLE</p>
+    
+                    <p>{booking.totalSpace} SERVICE AVAILABLE</p>
                     <button onClick={openModal} className="btn btn-brand text-uppercase">Book Appointment</button>
                     <OrderForm modalIsOpen={modalIsOpen} orderOn={booking.subject} date={date} closeModal={closeModal}/>
                 </div>
